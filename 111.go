967a56e6115e98e0825c9ba9ab83c806c0d513d7
liyuan2019 <li_yuan@retail-ai.jp>
2020-12-01 17:24:23 +0900

    test

---
+++ 111.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+// func main() {
+//     m := make(map[string]int)
+//     // var m map[string]int
+// 	m["one"] = 1 // error: panic: assignment to entry in nil map
+// 	fmt.Println(m)
+// }
+
+func main() {
+	var data interface{} = "great"
+
+	// // data 混用
+	// if data, ok := data.(int); ok {
+	// 	fmt.Println("[is an int], data: ", data)
+	// } else {
+	// 	fmt.Println("[not an int], data: ", data) // [isn't a int], data:  0
+	// }
+
+	if res, ok := data.(int); ok {
+		fmt.Println("[is an int], data: ", res)
+	} else {
+		fmt.Println("[not an int], data: ", data) // [not an int], data:  great
+	}
+}
