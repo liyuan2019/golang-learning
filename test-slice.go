2716580397b0c118162ad851e2bd0e54e53dc5ee
liyuan2019 <li_yuan@retail-ai.jp>
2020-12-01 14:48:04 +0900

    test-slice.go

--- test-slice.go
+++ test-slice.go
@@ -20,23 +20,40 @@ import "fmt"
 // 	fmt.Printf("2. a: %v\n", a)
 // }
 
+// func main() {
+// 	var a []int
+// 	for i := 0; i < 5; i++ {
+// 		a = append(a, i)
+// 	}
+
+// 	fmt.Printf("1. a: %v\n", a)
+
+// 	appendSlice(a)
+
+// 	fmt.Printf("3. a: %v\n", a)
+// }
+
+// func appendSlice(a []int) {
+// 	for i := 0; i < 5; i++ {
+// 		a = append(a, i)
+// 	}
+// 	a[2] = 9
+// 	fmt.Printf("2. a: %v\n", a)
+// }
+
+//微妙なケース
 func main() {
 	var a []int
 	for i := 0; i < 5; i++ {
 		a = append(a, i)
 	}
 
-	fmt.Printf("1. a: %v\n", a)
+	b := a
+	b = append(b, 5)
+	c := a
+	c = append(c, 6)
 
-	appendSlice(a)
-
-	fmt.Printf("3. a: %v\n", a)
-}
-
-func appendSlice(a []int) {
-	for i := 0; i < 5; i++ {
-		a = append(a, i)
-	}
-	a[2] = 9
-	fmt.Printf("2. a: %v\n", a)
+	fmt.Printf("a: %v\n", a)
+	fmt.Printf("b: %v\n", b)
+	fmt.Printf("c: %v\n", c)
 }
