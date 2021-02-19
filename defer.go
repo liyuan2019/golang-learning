31896871587de3b0a26896e32b09e1d70e42dc79
liyuan2019 <li_yuan@retail-ai.jp>
2021-02-19 15:14:20 +0900

    deferの中返り値変更test

--- defer.go
+++ defer.go
@@ -2,14 +2,28 @@ package main
 
 import "fmt"
 
+// func main() {
+// 	fmt.Println("counting")
+
+// 	for i := 0; i < 10; i++ {
+// 		defer fmt.Println(i)
+// 	}
+
+// 	defer fmt.Println("last defer")
+
+// 	fmt.Println("done")
+// }
+
+func test1() (myInt int) {
+	myInt = 1
+
+	defer func() {
+		myInt++
+	}()
+
+	return myInt
+}
+
 func main() {
-	fmt.Println("counting")
-
-	for i := 0; i < 10; i++ {
-		defer fmt.Println(i)
-	}
-
-	defer fmt.Println("last defer")
-
-	fmt.Println("done")
+	fmt.Printf("test1: %d\n", test1())
 }
