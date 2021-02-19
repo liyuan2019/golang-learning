e8cf3c5af130a48a8daaa9291e4c44184dc02b13
liyuan2019 <li_yuan@retail-ai.jp>
2021-02-19 15:15:02 +0900

    defer recover test

--- defer.go
+++ defer.go
@@ -14,16 +14,42 @@ import "fmt"
 // 	fmt.Println("done")
 // }
 
-func test1() (myInt int) {
-	myInt = 1
+// func test1() (myInt int) {
+// 	myInt = 1
 
-	defer func() {
-		myInt++
-	}()
+// 	defer func() {
+// 		myInt++
+// 	}()
 
-	return myInt
-}
+// 	return myInt
+// }
+
+// func main() {
+// 	fmt.Printf("test1: %d\n", test1())
+// }
 
 func main() {
-	fmt.Printf("test1: %d\n", test1())
+	f()
+	fmt.Println("Returned normally from f.")
+}
+
+func f() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in f", r)
+		}
+	}()
+	fmt.Println("Calling g.")
+	g(0)
+	fmt.Println("Returned normally from g.")
+}
+
+func g(i int) {
+	if i > 3 {
+		fmt.Println("Panicking!")
+		panic(fmt.Sprintf("%v", i))
+	}
+	defer fmt.Println("Defer in g", i)
+	fmt.Println("Printing in g", i)
+	g(i + 1)
 }
