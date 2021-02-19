4999f536a7bfb67899ab73dbeb8792efc55b983f
liyuan2019 <li_yuan@retail-ai.jp>
2021-02-19 15:35:56 +0900

    拓扑排序

--- sort/binsearch.go
+++ sort/binsearch.go
@@ -19,15 +19,15 @@ func bin_search(arr []int, finddata int) int {
 	return -1
 }
 
-func main() {
-	arr := make([]int, 1024*1024, 1024*1024)
-	for i := 0; i < 1024*1024; i++ {
-		arr[i] = i + 1
-	}
-	id := bin_search(arr, 1024)
-	if id != -1 {
-		fmt.Println(id, arr[id])
-	} else {
-		fmt.Println("没有找到数据")
-	}
-}
+// func main() {
+// 	arr := make([]int, 1024*1024, 1024*1024)
+// 	for i := 0; i < 1024*1024; i++ {
+// 		arr[i] = i + 1
+// 	}
+// 	id := bin_search(arr, 1024)
+// 	if id != -1 {
+// 		fmt.Println(id, arr[id])
+// 	} else {
+// 		fmt.Println("没有找到数据")
+// 	}
+// }
