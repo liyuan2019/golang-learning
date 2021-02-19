df2143e54f2501ddd837f23955338bb78dba37f0
liyuan2019 <li_yuan@retail-ai.jp>
2021-02-19 15:13:24 +0900

    defer 後入先出 test

---
+++ defer.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("counting")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+
+	defer fmt.Println("last defer")
+
+	fmt.Println("done")
+}
